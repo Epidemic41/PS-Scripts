ebfd55b9415058ce28da6b37a204927cd7b97e4d
Epidemic41 <75809107+Epidemic41@users.noreply.github.com>
2021-12-05 23:23:36 -0700

    Add files via upload

---
+++ splunk installation win.ps1
@@ -0,0 +1,20 @@
+cd \users\$env:USERNAME\Desktop
+
+curl -o SplunkinstallMSI1.msi https://download.splunk.com/products/splunk/releases/8.2.3/windows/splunk-8.2.3-cd0848707637-x64-release.msi -UseBasicParsing
+
+msiexec.exe /i splunk-8.2.3-cd0848707637-x64-release.msi 
+AGREETOLICENSE=Yes 
+INSTALLDIR="C:\Program Files\Splunk" 
+SPLUNKD_PORT=8089 
+WEB_PORT=8000 
+WINEVENTLOG_APP_ENABLE=1 
+WINEVENTLOG_SEC_ENABLE=1 
+WINEVENTLOG_SYS_ENABLE=1 
+WINEVENTLOG_FWD_ENABLE=1 
+WINEVENTLOG_SET_ENABLE=1 
+LAUNCHSPLUNK=1 
+INSTALL_SHORTCUT=1 
+SPLUNKUSERNAME=YourSideAdmin 
+SPLUNKPASSWORD=Ple@seB36ent@l
+
+//find out why msiexe isn't working
